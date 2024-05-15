8148dbeaf48c43e1f97e63ee49395d9b60034c2a
ahmadxzaky <ahmadxzaky@gmail.com>
2024-05-15 13:38:59 +0700

    kantin awal laravel

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,1 @@
+<p>This is Content user</p>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/tampilan', function(){
+    return view('tampilan');
+});
+
+Route::get('/', function () {
+    $data = [
+        'content' =>
+    ]
+    return view('admin.layouts.wrapper', $data);
+});
+
+Route::get('/user', function () {
+    $data = [
+        'content' => 'admin.user.index'
+    ];
+    return view('admin.layouts.wrapper', $data);
+});
+
+Route::get('/post', function () {
+    $data = [
+        'content' => 'admin.post.index'
+    ];
+    return view('admin.layouts.wrapper', $data);
+});
+
+
